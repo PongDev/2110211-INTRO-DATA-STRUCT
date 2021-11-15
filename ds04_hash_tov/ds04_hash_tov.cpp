1c99f792aab60433195ea49347e11128f94cec3f
PongDev <pongwiwat.dev@outlook.com>
2021-11-15 09:58:11 +0700

    2110211 INTRO DATA STRUCT - Grader Completion To Hash

---
+++ ds04_hash_tov/ds04_hash_tov.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+void hash_to_vector(unordered_map<int, string> hash, vector<int> &key, vector<string> &value) {
+    //write your code only here
+    for (auto data : hash) {
+        key.push_back(data.first);
+        value.push_back(data.second);
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+    unordered_map<int, string> hash;
+    while (n--) {
+        int a;
+        string st;
+        cin >> a >> st;
+        hash[a] = st;
+    }
+
+    vector<int> k;
+    vector<string> v;
+    hash_to_vector(hash, k, v);
+    for (auto &x : k)
+        cout << x << " ";
+    cout << endl;
+    for (auto &x : v)
+        cout << x << " ";
+    cout << endl;
+}
